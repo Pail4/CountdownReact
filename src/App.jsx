c1ac4841ab8e97785ec8b0416d68facec2f70e6f
Pail4 <73517684+Pail4@users.noreply.github.com>
2022-03-11 10:38:04 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import './App.css'
+import React from 'react'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        App
+      </div>
+    );
+  }
+}
+
+export default App
